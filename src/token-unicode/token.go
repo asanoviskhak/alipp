3d075e42da7c91333df812136999a15e7eff79c9
Iskhak Asanov <asanov.personal@gmail.com>
2024-07-22 17:26:42 +0600

    :tada: Added unicode support

---
+++ src/token-unicode/token.go
@@ -0,0 +1,69 @@
+package tokenunicode
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ТУУРА_ЭМЕС"
+	EOF     = "БҮТТҮ"
+	IDENT   = "ИДЕНТИФИКАТОР"
+	INT     = "БҮТҮН_САН"
+
+	// Operators
+	ASSIGN      = "="
+	PLUS        = "+"
+	MINUS       = "-"
+	EXCLAMATION = "!"
+	ASTERISK    = "*"
+	SLASH       = "/"
+	LT          = "<"
+	GT          = ">"
+	EQ          = "=="
+	NOT_EQ      = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "ФУНКЦИЯ"
+	LET      = "БЕР"
+	TRUE     = "ТУУРА"
+	FALSE    = "КАТА"
+	IF       = "ЭГЕР"
+	ELSE     = "ЖЕ"
+	RETURN   = "КАЙТАР"
+
+	// Excerpt From
+	// Writing An Interpreter In Go
+	// Thorsten Ball
+	// This material may be protected by copyright.
+)
+
+var keywords = map[string]TokenType{
+	"функция": FUNCTION,
+	"функ":    FUNCTION,
+	"бер":     LET,
+	"туура":   TRUE,
+	"ката":    FALSE,
+	"эгер":    IF,
+	"же":      ELSE,
+	"кайтар":  RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
